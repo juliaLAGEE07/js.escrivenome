0e6c19ba23ea94e8386add868eead064985b2f93
juliaLAGEE07 <00001110897595sp@al.educacao.sp.gov.br>
2024-06-12 10:35:19 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas(400, 400); // width x height
+  background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  circuloX = [0,0,0];
+  circuloY = [random(height), random(height), random(height)];
+}
